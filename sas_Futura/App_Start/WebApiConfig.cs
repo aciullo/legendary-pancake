a10840f5e41d86646cca17c8fa83e10c411e8727
aciullo <andrea.ciullo@hotmail.com>
2016-06-10 10:52:09 -0400

    Web API solution

---
+++ sas_Futura/App_Start/WebApiConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace sas_Futura
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}/{idestado}/{estado}",
+                defaults: new { id = RouteParameter.Optional, idestado = RouteParameter.Optional, estado = RouteParameter.Optional }
+            );
+
+
+            config.Routes.MapHttpRoute(
+                name: "DeviceUsersApi",
+                routeTemplate: "api/{controller}/{id}/{param2}",
+                defaults: new { id = RouteParameter.Optional, param2 = RouteParameter.Optional }
+            );
+
+            var json = config.Formatters.JsonFormatter;
+            json.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.Objects;
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+        }
+    }
+}
---
+++ sas_Futura/Models/DeviceUser.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace sas_Futura.Models
+{
+    public class DeviceUserContext : DbContext
+    {
+        public DeviceUserContext()
+            : base("FuturaConnection")
+        {
+            Database.SetInitializer<ServiciosContext>(null);
+        }
+
+        public virtual DbSet<DeviceUser> DeviceUser { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Configure default schema
+            modelBuilder.HasDefaultSchema("dbo");
+
+            //Map entity to table
+            modelBuilder.Entity<DeviceUser>().ToTable("DeviceUser");
+
+
+        }
+
+    }
+     [Table("DeviceUser")]
+    public class DeviceUser
+    {
+
+           [Key]
+        [Column("UserName")]
+        public string usuario { get; set; }
+          [Column("password")]
+        public string pass { get; set; }
+
+        [Column("idMovil")]
+        public string codMovil { get; set; }
+
+        public string nombres { get; set; }
+        public string apellidos { get; set; }
+
+        public string idRegistro { get; set; }
+
+    }
+}
